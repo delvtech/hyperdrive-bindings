2b3db6c63c5144af550d27770319b497a672365f
Dylan Paiton <dpaiton@users.noreply.github.com>
2023-08-22 15:05:54 -0700

    Adds functions to support direct access to get_max functions (#2)

--- crates/pyperdrive/python/pyperdrive/pyperdrive.pyi
+++ crates/pyperdrive/python/pyperdrive/pyperdrive.pyi
@@ -3,6 +3,8 @@ from __future__ import annotations
 
 from . import types
 
+# pylint: disable=unused-argument
+
 class HyperdriveState:
     """A class representing the hyperdrive contract state."""
 
@@ -30,7 +32,7 @@ class HyperdriveState:
         """Gets the max amount of bonds that can be purchased for the given budget.
 
         Arguments
-        ----------
+        ---------
         budget : str
             The account budget in base for making a long.
         maybe_max_iterations : int
@@ -45,7 +47,7 @@ class HyperdriveState:
         """Gets the max amount of bonds that can be shorted for the given budget.
 
         Arguments
-        ----------
+        ---------
         budget : str
             The account budget in base for making a short.
         open_share_price : str
@@ -58,3 +60,75 @@ class HyperdriveState:
         str
             The maximum short as a string representation of a solidity uint256 value.
         """
+
+def get_max_long(
+    pool_config: types.PoolConfig,
+    pool_info: types.PoolInfo,
+    budget: str,
+    maybe_max_iterations: int,
+) -> str:
+    """Gets the max amount of bonds that can be purchased for the given budget.
+
+    Arguments
+    ---------
+    pool_config : PoolConfig
+        Static configuration for the hyperdrive contract.  Set at deploy time.
+    pool_info : PoolInfo
+        Current state information of the hyperdrive contract.  Includes things like reserve levels and share prices.
+    budget : str
+        The account budget in base for making a long.
+    maybe_max_iterations : int
+        The number of iterations to use for the Newtonian method.
+
+    Returns
+    -------
+    str
+        The maximum long as a string representation of a solidity uint256 value.
+    """
+
+def get_max_short(
+    pool_config: types.PoolConfig,
+    pool_info: types.PoolInfo,
+    budget: str,
+    open_share_price: str,
+    maybe_max_iterations: int,
+) -> str:
+    """Gets the max amount of bonds that can be shorted for the given budget.
+
+    Arguments
+    ---------
+    pool_config : PoolConfig
+        Static configuration for the hyperdrive contract.  Set at deploy time.
+    pool_info : PoolInfo
+        Current state information of the hyperdrive contract.  Includes things like reserve levels and share prices.
+    budget : str
+        The account budget in base for making a short.
+    open_share_price : str
+        The share price of underlying vault.
+    maybe_max_iterations : int
+        The number of iterations to use for the Newtonian method.
+
+    Returns
+    -------
+    str
+        The maximum short as a string representation of a solidity uint256 value.
+    """
+
+def get_spot_price(
+    pool_config: types.PoolConfig,
+    pool_info: types.PoolInfo,
+) -> str:
+    """Gets the spot price of the bond.
+
+    Arguments
+    ---------
+    pool_config : PoolConfig
+        Static configuration for the hyperdrive contract.  Set at deploy time.
+    pool_info : PoolInfo
+        Current state information of the hyperdrive contract.  Includes things like reserve levels and share prices.
+
+    Returns
+    -------
+    str
+        The spot price as a string representation of a solidity uint256 value.
+    """
--- system_tests/wrapper_tests.py
+++ system_tests/wrapper_tests.py
@@ -1,8 +1,10 @@
 """Tests for hyperdrive_math.rs wrappers"""
 import pytest
 
-from pyperdrive import HyperdriveState  # pylint: disable=no-name-in-module
+import pyperdrive
 from pyperdrive.types import Fees, PoolConfig, PoolInfo
+
+# pylint: disable=no-member
 
 sample_pool_config = PoolConfig(
     base_token="0x1234567890abcdef1234567890abcdef12345678",
@@ -38,30 +40,41 @@ sample_pool_info = PoolInfo(
 
 def test_initialization():
     """test initialization."""
-    state = HyperdriveState(sample_pool_config, sample_pool_info)
+    state = pyperdrive.HyperdriveState(sample_pool_config, sample_pool_info)
     assert state is not None, "State initialization failed."
 
 
 def test_get_spot_price():
     """test get_spot_price."""
-    state = HyperdriveState(sample_pool_config, sample_pool_info)
+    state = pyperdrive.HyperdriveState(sample_pool_config, sample_pool_info)
     spot_price = state.get_spot_price()
     assert spot_price is not None, "Failed to get spot price."
     assert isinstance(spot_price, str), "Expected spot price to be a string."
+    assert int(spot_price) > 0, "Expected spot price to > 0."
+    # test the helper function
+    assert pyperdrive.get_spot_price(sample_pool_config, sample_pool_info) == spot_price
 
 
 def test_max_long():
     """test get_max_long."""
-    state = HyperdriveState(sample_pool_config, sample_pool_info)
+    state = pyperdrive.HyperdriveState(sample_pool_config, sample_pool_info)
     budget = "1000000000000000000"  # 1 base
     max_iterations = 20
     max_long = state.get_max_long(budget, max_iterations)
     assert int(max_long) > 0  # should == "1000000000000000000", or 1 base
+    # test the helper function
+    max_long_direct = pyperdrive.get_max_long(
+        sample_pool_config,
+        sample_pool_info,
+        budget,
+        max_iterations,
+    )
+    assert max_long_direct == max_long
 
 
 def test_max_long_fail_conversion():
     """test get_max_long."""
-    state = HyperdriveState(sample_pool_config, sample_pool_info)
+    state = pyperdrive.HyperdriveState(sample_pool_config, sample_pool_info)
     max_iterations = 20
 
     # bad string
@@ -77,17 +90,27 @@ def test_max_long_fail_conversion():
 
 def test_max_short():
     """test get_max_short."""
-    state = HyperdriveState(sample_pool_config, sample_pool_info)
+    # test using the state directly
+    state = pyperdrive.HyperdriveState(sample_pool_config, sample_pool_info)
     budget = "10000000000000000000000"  # 10k base
     open_share_price = "1000000000000000000"  # 1 base
     max_iterations = 20
     max_short = state.get_max_short(budget, open_share_price, max_iterations)
     assert int(max_short) > 0  # should == "2583754033693357393077", or apprx 2583 base
+    # test the helper function
+    max_short_direct = pyperdrive.get_max_short(
+        sample_pool_config,
+        sample_pool_info,
+        budget,
+        open_share_price,
+        max_iterations,
+    )
+    assert max_short_direct == max_short
 
 
 def test_max_short_fail_conversion():
     """test get_max_short."""
-    state = HyperdriveState(sample_pool_config, sample_pool_info)
+    state = pyperdrive.HyperdriveState(sample_pool_config, sample_pool_info)
     open_share_price = "1000000000000000000"  # 1 base
     max_iterations = 20
 
@@ -110,7 +133,7 @@ def test_max_short_fail_conversion():
 
 def test_max_short_fail_budge():
     """test get_max_short."""
-    state = HyperdriveState(sample_pool_config, sample_pool_info)
+    state = pyperdrive.HyperdriveState(sample_pool_config, sample_pool_info)
     open_share_price = "1000000000000000000"  # 1 base
     max_iterations = 20
 
