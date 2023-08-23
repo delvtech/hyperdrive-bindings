6b57c0f95513f444eed2dc152bf98818726d4e3c
Dylan <dpaiton@gmail.com>
2023-08-23 07:51:45 -0700

    new build process for linux

---
+++ scripts/build_pyperdrive.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Pyperdrive install for Linux-based platforms
+# This is a temporary script until we finish seting up cibuildwheel for linux
+# The script assumes that the `hyperdrive` repo is cloned inside this repository root
+
+
+echo "install required packages for building wheels"
+python -m pip install --upgrade -r requirements-dev.txt
+
+echo "nav into the crate so relative paths work"
+cd crates/pyperdrive
+
+echo "build the wheel for the current platform"
+python setup.py bdist_wheel
+
+echo "nav back to project root"
+cd ../.. 
+
+echo "copy built wheel files from pyperdrive into packages"
+cp crates/pyperdrive/dist/* .wheelhouse/
