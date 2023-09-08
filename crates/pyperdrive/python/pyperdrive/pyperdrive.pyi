6d3a1c7597b10e775033e459d30cff43fa7ec117
Dylan <dpaiton@gmail.com>
2023-09-08 13:33:55 -0700

    updates pyperdrive to match hyperdrive-rs

--- crates/pyperdrive/python/pyperdrive/pyperdrive.pyi
+++ crates/pyperdrive/python/pyperdrive/pyperdrive.pyi
@@ -4,6 +4,7 @@ from __future__ import annotations
 from . import types
 
 # pylint: disable=unused-argument
+# pylint: disable=too-many-arguments
 
 class HyperdriveState:
     """A class representing the hyperdrive contract state."""
@@ -21,7 +22,7 @@ class HyperdriveState:
             Current state information of the hyperdrive contract.  Includes things like reserve levels and share prices.
         """
     def get_spot_price(self) -> str:
-        """Gets the spot price of the bond.
+        """Get the spot price of the bond.
 
         Returns
         -------
@@ -29,7 +30,7 @@ class HyperdriveState:
             The spot price as a string representation of a solidity uint256 value.
         """
     def get_max_long(self, budget: str, maybe_max_iterations: int) -> str:
-        """Gets the max amount of bonds that can be purchased for the given budget.
+        """Get the max amount of bonds that can be purchased for the given budget.
 
         Arguments
         ---------
@@ -44,7 +45,7 @@ class HyperdriveState:
             The maximum long as a string representation of a solidity uint256 value.
         """
     def get_max_short(self, budget: str, open_share_price: str, maybe_max_iterations: int) -> str:
-        """Gets the max amount of bonds that can be shorted for the given budget.
+        """Get the max amount of bonds that can be shorted for the given budget.
 
         Arguments
         ---------
@@ -67,16 +68,18 @@ def get_max_long(
     budget: str,
     maybe_max_iterations: int,
 ) -> str:
-    """Gets the max amount of bonds that can be purchased for the given budget.
+    """Get the max amount of bonds that can be purchased for the given budget.
 
     Arguments
     ---------
     pool_config : PoolConfig
         Static configuration for the hyperdrive contract.  Set at deploy time.
     pool_info : PoolInfo
-        Current state information of the hyperdrive contract.  Includes things like reserve levels and share prices.
-    budget : str
+        Current state information of the hyperdrive contract. Includes things like reserve levels and share prices.
+    budget : str (FixedPoint)
         The account budget in base for making a long.
+    checkpoint_exposure : int
+        The net exposure for the given checkpoint.
     maybe_max_iterations : int
         The number of iterations to use for the Newtonian method.
 
@@ -91,9 +94,10 @@ def get_max_short(
     pool_info: types.PoolInfo,
     budget: str,
     open_share_price: str,
+    maybe_conservative_price: str,
     maybe_max_iterations: int,
 ) -> str:
-    """Gets the max amount of bonds that can be shorted for the given budget.
+    """Get the max amount of bonds that can be shorted for the given budget.
 
     Arguments
     ---------
@@ -101,10 +105,12 @@ def get_max_short(
         Static configuration for the hyperdrive contract.  Set at deploy time.
     pool_info : PoolInfo
         Current state information of the hyperdrive contract.  Includes things like reserve levels and share prices.
-    budget : str
+    budget : str (FixedPoint)
         The account budget in base for making a short.
-    open_share_price : str
+    open_share_price : str (FixedPoint)
         The share price of underlying vault.
+    maybe_conservative_price : str (FixedPoint)
+        A lower bound on the realized price that the short will pay.
     maybe_max_iterations : int
         The number of iterations to use for the Newtonian method.
 
@@ -118,7 +124,7 @@ def get_spot_price(
     pool_config: types.PoolConfig,
     pool_info: types.PoolInfo,
 ) -> str:
-    """Gets the spot price of the bond.
+    """Get the spot price of the bond.
 
     Arguments
     ---------
