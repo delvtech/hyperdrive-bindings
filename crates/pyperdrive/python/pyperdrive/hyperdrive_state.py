4ae11cda75e9df36978f94a9654edaf71f613973
Dylan <dpaiton@gmail.com>
2023-10-26 01:05:34 -0700

    reorg wrapper

--- crates/pyperdrive/python/pyperdrive/hyperdrive_state.py
+++ crates/pyperdrive/python/pyperdrive/hyperdrive_state.py
@@ -1,4 +1,4 @@
-"""Python wrapper for the rust module."""
+"""Python wrapper for the rust hyperdrive_math::State module."""
 from __future__ import annotations
 
 # pylint: disable=no-name-in-module
@@ -12,7 +12,9 @@ from .utils import _serialize_pool_config, _serialize_pool_info
 # pylint: disable=too-many-arguments
 
 
-## Hyperdrive State functions
+## Hyperdrive state functions.
+
+
 def get_solvency(
     pool_config: types.PoolConfigType,
     pool_info: types.PoolInfoType,
@@ -390,91 +392,3 @@ def get_in_for_out(
         pool_config_serialized, pool_info_serialized
     )
     return rust_interface.get_in_for_out(amount_out, shares_out)
-
-
-## hyperdrie_math utilities
-
-
-def get_time_stretch(rate: str) -> str:
-    """Calculate the time stretch parameter given a pool's spot rate.
-
-    ..math::
-        tau = 5.24592 / (0.4665 * r * 100)
-
-    Arguments
-    ---------
-    rate : str (FixedPoint)
-        The pool's spot rate (aka apr, or fixed rate).
-
-    Returns
-    -------
-    time_stretch : str (FixedPoint)
-        The time stretch parameter (tau).
-    """
-    return rust_module.get_time_stretch(rate)
-
-
-def get_effective_share_reserves(
-    share_reserves: str,
-    share_adjustment: str,
-) -> str:
-    r"""Calculate the effective share reserves given the share reserves and share adjustment.
-
-    ..math::
-        z_effective = z - \zeta
-
-    Arguments
-    ---------
-    share_reserves : str (FixedPoint)
-        The pool's share reserves.
-    share_adjustment : str (I256)
-        The zeta factor for adjusting share reserves.
-
-    Returns
-    -------
-    effective_share_reserves : str (FixedPoint)
-        The adjusted share reserves, accounting for the zeta factor.
-    """
-    return rust_module.get_effective_share_reserves(share_reserves, share_adjustment)
-
-
-def calculate_bonds_given_shares_and_rate(
-    effective_share_reserves: str,
-    initial_share_price: str,
-    apr: str,
-    position_duration: str,
-    time_stretch: str,
-) -> str:
-    """
-    Calculates the bond reserves assuming that the pool has a given
-    share reserves and fixed rate APR.
-
-    ..math::
-        r = ((1/p)-1)/t = (1-p)/(pt)
-        p = ((u * z) / y) ** t
-        y = mu * (z - zeta) * (1 + apr * t) ** (1/tau)
-
-    Arguments
-    ---------
-    effective_share_reserves : str (FixedPoint)
-        The pool's effective share reserves. The effective share
-        reserves are a modified version of the share reserves
-        used when pricing trades.
-    initial_share_price : str (FixedPoint)
-        The pool's initial share price.
-    apr : str (FixedPoint)
-        The pool's APR.
-    position_duration : str (FixedPoint)
-        The amount of time until maturity in seconds.
-    time_stretch : str (FixedPoint)
-        The time stretch parameter (tau).
-
-    Returns
-    -------
-    bond_reserves : str (FixedPoint)
-        The bond reserves (without adjustment) that make
-        the pool have a specified APR.
-    """
-    return rust_module.calculate_bonds_given_shares_and_rate(
-        effective_share_reserves, initial_share_price, apr, position_duration, time_stretch
-    )
