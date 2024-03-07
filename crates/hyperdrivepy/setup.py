302fc2b60a0af34b5d658c72f05ee00baf761cc0
Sheng Lundquist <sheng@delv.tech>
2024-03-07 15:28:03 -0800

    Actual version bump

--- crates/hyperdrivepy/setup.py
+++ crates/hyperdrivepy/setup.py
@@ -5,7 +5,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="hyperdrivepy",
-    version="0.8.0",
+    version="0.9.0",
     packages=["hyperdrivepy"],
     package_dir={"": "python"},
     rust_extensions=[
