23b497e31ecce065ef6c42366ee9180603ac3063
Sheng Lundquist <slundqui007@gmail.com>
2024-03-07 17:22:45 -0800

    Update hyperdrivepy to v0.9.0 (#69)

--- crates/hyperdrivepy/src/hyperdrive_state_methods.rs
+++ crates/hyperdrivepy/src/hyperdrive_state_methods.rs
@@ -158,9 +158,10 @@ impl HyperdriveState {
         let amount_in_fp = FixedPoint::from(U256::from_dec_str(amount_in).map_err(|_| {
             PyErr::new::<PyValueError, _>("Failed to convert amount_in string to U256")
         })?);
+        // We unwrap the error here to throw panic error if this fails
         let result_fp = self
             .state
-            .calculate_shares_in_given_bonds_out_up(amount_in_fp);
+            .calculate_shares_in_given_bonds_out_up_safe(amount_in_fp).unwrap();
         let result = U256::from(result_fp).to_string();
         return Ok(result);
     }
