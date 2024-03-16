a787b16be54ca9c5c1c1071856c485174f63a811
Sheng Lundquist <sheng@delv.tech>
2024-03-15 21:36:46 -0700

    Adding in checks for running build wheels only when pushed to main and version increments

--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # hyperdrivepy install for Linux-based platforms
-# This is a temporary script until we finish seting up cibuildwheel for linux
-# The script assumes that the `hyperdrive` repo is cloned inside this repository root
-
 
 echo "install required packages for building wheels"
 python -m pip install --upgrade -r requirements-dev.txt
@@ -18,4 +15,5 @@ python setup.py bdist_wheel
 echo "repair built wheel to be manylinux into wheelhouse"
 # wheelhouse is in the package root
 mkdir ../../wheelhouse 
+# Make the image manylinux complient
 auditwheel repair dist/hyperdrivepy-*-linux_x86_64.whl -w ../../wheelhouse --plat manylinux_2_34_x86_64
