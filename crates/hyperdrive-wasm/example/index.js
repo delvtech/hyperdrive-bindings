708909361098d5e15f31966091a682725ef8e353
Ryan Goree <goree.ryan@gmail.com>
2024-04-04 18:44:29 -0500

    Copy prettier from monorepo

--- crates/hyperdrive-wasm/example/index.js
+++ crates/hyperdrive-wasm/example/index.js
@@ -1,44 +1,44 @@
-import * as hyperdriveWasm from '@delvtech/hyperdrive-wasm';
+import * as hyperdriveWasm from "@delvtech/hyperdrive-wasm";
 
-const ZERO_ADDRESS = '0x'.padEnd(42, '0');
-const MAX_U256 = '0x'.padEnd(66, 'F');
+const ZERO_ADDRESS = "0x".padEnd(42, "0");
+const MAX_U256 = "0x".padEnd(66, "F");
 const MAX_BUDGET = BigInt(MAX_U256).toString();
 
 const examplePoolInfo = {
-  shareReserves: '10000000000000000000000000',
-  shareAdjustment: '0',
-  zombieBaseProceeds: '0',
-  zombieShareReserves: '0',
-  bondReserves: '10217899519533796120000000',
-  lpTotalSupply: '9999990000000000000000000',
-  vaultSharePrice: '1000000000000000000',
-  sharePrice: '1000000000000000000',
-  longsOutstanding: '0',
-  longAverageMaturityTime: '0',
-  shortsOutstanding: '0',
-  shortAverageMaturityTime: '0',
-  withdrawalSharesReadyToWithdraw: '0',
-  withdrawalSharesProceeds: '0',
-  lpSharePrice: '1000000000000000000',
-  longExposure: '0',
+  shareReserves: "10000000000000000000000000",
+  shareAdjustment: "0",
+  zombieBaseProceeds: "0",
+  zombieShareReserves: "0",
+  bondReserves: "10217899519533796120000000",
+  lpTotalSupply: "9999990000000000000000000",
+  vaultSharePrice: "1000000000000000000",
+  sharePrice: "1000000000000000000",
+  longsOutstanding: "0",
+  longAverageMaturityTime: "0",
+  shortsOutstanding: "0",
+  shortAverageMaturityTime: "0",
+  withdrawalSharesReadyToWithdraw: "0",
+  withdrawalSharesProceeds: "0",
+  lpSharePrice: "1000000000000000000",
+  longExposure: "0",
 };
 const examplePoolConfig = {
   baseToken: ZERO_ADDRESS,
   governance: ZERO_ADDRESS,
   feeCollector: ZERO_ADDRESS,
   fees: {
-    curve: '100000000000000000',
-    flat: '500000000000000',
-    governanceLP: '10000000000000000',
-    governanceZombie: '100000000000000000',
+    curve: "100000000000000000",
+    flat: "500000000000000",
+    governanceLP: "10000000000000000",
+    governanceZombie: "100000000000000000",
   },
-  initialVaultSharePrice: '1000000000000000000',
-  minimumShareReserves: '10000000000000000000',
-  minimumTransactionAmount: '1000000000000000',
-  timeStretch: '44463125629060298',
-  positionDuration: '604800',
-  checkpointDuration: '3600',
-  linkerCodeHash: '0x'.padEnd(66, '0'),
+  initialVaultSharePrice: "1000000000000000000",
+  minimumShareReserves: "10000000000000000000",
+  minimumTransactionAmount: "1000000000000000",
+  timeStretch: "44463125629060298",
+  positionDuration: "604800",
+  checkpointDuration: "3600",
+  linkerCodeHash: "0x".padEnd(66, "0"),
   linkerFactory: ZERO_ADDRESS,
   sweepCollector: ZERO_ADDRESS,
   vaultSharesToken: ZERO_ADDRESS,
@@ -48,28 +48,28 @@ async function main() {
   hyperdriveWasm.initSync(hyperdriveWasm.wasmBuffer);
 
   const spotRate = hyperdriveWasm.spotRate(examplePoolInfo, examplePoolConfig);
-  console.log('spotRate:', spotRate);
+  console.log("spotRate:", spotRate);
 
   const maxLong = hyperdriveWasm.maxLong(
     examplePoolInfo,
     examplePoolConfig,
     MAX_BUDGET,
-    '90844806244066488'
+    "90844806244066488",
   );
-  console.log('maxLong:', maxLong);
+  console.log("maxLong:", maxLong);
 
   const baseForMaxLong = hyperdriveWasm.calcOpenLong(
     examplePoolInfo,
     examplePoolConfig,
-    maxLong
+    maxLong,
   );
-  console.log('baseForMaxLong:', baseForMaxLong);
+  console.log("baseForMaxLong:", baseForMaxLong);
 
   const spotPrice = hyperdriveWasm.spotPrice(
     examplePoolInfo,
-    examplePoolConfig
+    examplePoolConfig,
   );
-  console.log('spotPrice:', spotPrice);
+  console.log("spotPrice:", spotPrice);
 
   // Returns: 15977080231906768517
   // const openShortPreview = hyperdriveWasm.calcOpenShort(
@@ -83,13 +83,13 @@ async function main() {
     examplePoolInfo,
     examplePoolConfig,
     (10_000n * 10n ** 18n).toString(),
-    '900000000000000000'
+    "900000000000000000",
   );
-  console.log('openShortPreview', openShortPreview);
+  console.log("openShortPreview", openShortPreview);
 
   const shortAmount = 10_000n * 10n ** 18n;
   const currentTime = Math.floor(
-    examplePoolConfig.positionDuration * Math.random()
+    examplePoolConfig.positionDuration * Math.random(),
   );
   const feeArgs = [
     examplePoolInfo,
@@ -99,16 +99,16 @@ async function main() {
     currentTime.toString(),
   ];
 
-  console.log('feeArgs:', feeArgs);
+  console.log("feeArgs:", feeArgs);
 
   const closeShortCurveFee = hyperdriveWasm.closeShortCurveFee(...feeArgs);
-  console.log('closeShortCurveFee:', closeShortCurveFee);
+  console.log("closeShortCurveFee:", closeShortCurveFee);
 
   const closeShortFlatFee = hyperdriveWasm.closeShortFlatFee(...feeArgs);
-  console.log('closeShortFlatFee:', closeShortFlatFee);
+  console.log("closeShortFlatFee:", closeShortFlatFee);
 
   const openShortCurveFee = hyperdriveWasm.openShortCurveFee(...feeArgs);
-  console.log('openShortCurveFee:', openShortCurveFee);
+  console.log("openShortCurveFee:", openShortCurveFee);
 }
 
 main();
