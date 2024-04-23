9e8f4bd292cd8e36e14494bc63882af0fb38abf4
Danny Delott <dannydelott@gmail.com>
2024-04-22 18:01:26 -0700

    wrap method for calcImpliedRate in hyperwasm

--- crates/hyperdrive-wasm/src/short/open.rs
+++ crates/hyperdrive-wasm/src/short/open.rs
@@ -1,4 +1,4 @@
-use ethers::types::U256;
+use ethers::types::{I256, U256};
 use fixed_point::FixedPoint;
 use hyperdrive_math::State;
 use wasm_bindgen::prelude::wasm_bindgen;
@@ -67,4 +67,40 @@ pub fn spotPriceAfterShort(
         .unwrap();
 
     U256::from(result_fp).to_string()
+}
+
+/// Calculate the implied rate of opening a short at a given size. This rate
+/// is calculated as an APY.
+/// @param poolInfo - The current state of the pool
+///
+/// @param poolConfig - The pool's configuration
+///
+/// @param bondAmount - The amount of bonds to short
+///
+/// @param openVaultSharePrice - The vault share price at the start of the
+/// checkpoint
+///
+/// @param variableApy - The variable apy
+#[wasm_bindgen(skip_jsdoc)]
+pub fn calcImpliedRate(
+    poolInfo: &JsPoolInfo,
+    poolConfig: &JsPoolConfig,
+    bondAmount: &str,
+    openVaultSharePrice: &str,
+    variableApy: &str,
+) -> String {
+    set_panic_hook();
+    let state = State {
+        info: poolInfo.into(),
+        config: poolConfig.into(),
+    };
+    let bond_amount = FixedPoint::from(U256::from_dec_str(bondAmount).unwrap());
+    let open_vault_share_price = FixedPoint::from(U256::from_dec_str(openVaultSharePrice).unwrap());
+    let variable_apy = FixedPoint::from(U256::from_dec_str(variableApy).unwrap());
+
+    let result_fp = state
+        .calculate_implied_rate(bond_amount, open_vault_share_price, variable_apy)
+        .unwrap();
+
+    I256::from(result_fp).to_string()
 }
